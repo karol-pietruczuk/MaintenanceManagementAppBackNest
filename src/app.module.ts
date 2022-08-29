0957aabaaaf9123e7157ad292fa452743faa21a6
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-08-30 01:07:06 +0200

    Typeorm configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { dataSourceOptions } from "./config/config";
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.example.ts
@@ -0,0 +1,15 @@
+//
+// import { DataSourceOptions } from "typeorm";
+//
+// export const config: DataSourceOptions = {
+//   type: "mysql",
+//   host: "localhost",
+//   port: 3306,
+//   username: "root",
+//   password: "",
+//   database: "maintence_managment-app",
+//   entities: ["dist/**/**.entity{.ts,.js}"],
+//   bigNumberStrings: false,
+//   logging: true,
+//   migrations: ['dist/migration/**{.ts,.js}'],
+// }
